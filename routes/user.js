482fa14be3cb77561f9fa91d206b03aca59a66a8
Ogogor Akachukwu <118525443+akachi11@users.noreply.github.com>
2024-03-30 15:48:41 +0100

    Add files via upload

--- routes/user.js
+++ routes/user.js
@@ -39,6 +39,22 @@ router.get("/find/:id", async (req, res) => {
     }
 });
 
+// FORM CHECK
+router.get("/find-user", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email })
+        if (user == null) {
+            const next = await User.findOne({ username: req.body.username })
+            res.status(200).json(next)
+        } else {
+            res.status(200).json(user)
+        }
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 //GET ALL USERS
 router.get("/find-all", async (req, res) => {
     const query = req.query.new
